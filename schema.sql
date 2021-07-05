e867449501b8436ab20ccdc75852d902b84dcbd4
Dana Merrill <dtmerrilliii@gmail.com>
2021-07-05 14:10:15 -0400

    format updates

--- schema.sql
+++ schema.sql
@@ -4,30 +4,31 @@ CREATE database all_ee_db;
 USE all_ee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NULL,
+  PRIMARY KEY (id),
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE work_role (
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(100) NULL,
   salary DECIMAL NULL,
   department_id INT NULL,
-
+  PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
   manager_id INT NULL,
-
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES work_role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 SELECT * FROM employee; 
-SELECT * FROM role; 
+SELECT * FROM work_role; 
 SELECT * FROM department; 
--- seed.sql
+++ seed.sql
@@ -1,9 +1,9 @@
 USE all_ee_db;
 
-INSERT INTO department (name)
+INSERT INTO department (dept_name)
 VALUES ('Direction'), ('Acting'), ('Management'), ('Lighting'), ('Sound'), ('Sets');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO work_role (title, salary, department_id)
 VALUES ('Director', 100000, 1), ('Assistant Director', 40000, 1), ('Lead Actor', 150000, 2), ('Understudy', 60000, 2), ('Actor', 90000, 2), ('Stage Manager', 70000, 3), ('Assistant Stage Manager', 30000, 3), ('Stage Hand', 20000, 3), ('Lighting Designer', 80000, 4), ('Electrician', 50000, 4), ('Techie', 20000, 4), ('Sound Designer', 70000, 5), ('Sound Technician', 20000, 5), ('Set Designer', 90000, 6), ('Construction Lead', 60000, 6), ('Set Builder', 20000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
